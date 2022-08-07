f36f8819fa395d834051be801b9edc1386ddd9dc
Angel Lopez <srterradev@gmail.com>
2022-08-06 22:55:12 -0600

    deploy with portss

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+require("dotenv").config();
+
 // Express
 const express = require("express");
 const path = require("path");
 
 // INITIALIZING
 const app = express();
-app.set("port", 8080);
 
 // STATIC FILES
 app.use(express.static(path.join(__dirname, "public")));
 
 // RUN SERVER
-app.listen(app.get("port"), () => {
-  console.log("SERVER ON PORT: ", app.get("port"));
+app.listen(process.env.PORT || 8080, "0.0.0.0", () => {
+  console.log("Server is running...");
 });
